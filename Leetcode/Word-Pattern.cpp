66d387503583b1a30e2bb602dea67c855b214b99
HaomingMei <137435133+HaomingMei@users.noreply.github.com>
2025-07-14 17:05:04 -0500

    [290] [Time Beats: 100.00%] [Memory Beats: 68.82%] - LeetPush

--- Leetcode/Word-Pattern.cpp
+++ Leetcode/Word-Pattern.cpp
@@ -1,78 +1,57 @@
-#include <iostream>
-#include <unordered_map>
 #include <string>
+#include <unordered_map>
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        // Implementation: Dual hashmap that crosschecks to ensure
-        // bijection mappings
-        //std::cout << pattern[0] << endl;
-        //std::cout << s[0] << endl;
+        std::unordered_map<char, std::string> hashmap1; // Character to Word
+        std::unordered_map<std::string, char> hashmap2; // Word to Character
 
-        // Setup pointer to characters of pattern
-        char * current_char = &pattern[0];
-       // std::cout << *current_char << endl;
-        //current_char++;
-        //std::cout << *current_char << endl;
-
-        char* i = &s[0];
-        // This outputs the address
-            //std::cout << static_cast<void*>(&s[0]) << endl;
-        char* j = i;
-        // Case where either of the string is NULL
-     
-        // Create the hashmaps
-        std::unordered_map<char, std::string> char_to_word_map;
-        std::unordered_map<std::string, char> word_to_char_map;
-        char * char_to_word_end = i + s.size(); // Include the NULL terminator
-        char * last_char = current_char + pattern.size();
-        std::cout << "Size of string s is " << s.size() << endl;
-        // Iterating through the s string
-        std::cout << "The ending address of the char_to_word_end is " << static_cast<void *>(char_to_word_end) << "\n" << endl;
-        while(j != char_to_word_end+1 && current_char != last_char ){
-
-            // Case 1: j is at "_", meaning we found a word
-            std::cout << "The value of j is " << *j << endl;
-            std::cout << "The address of j is " << static_cast<void *>(j) << "\n" << endl;
-
-            if(*(j+1) == ' ' || j+1 == char_to_word_end){
-                // Have a word
-                
-                string temp(i, j-i+1); // Compies the string without the null terminator
+        int i = 0; // Iterator for characters (pattern)
+        int j = 0; // Iterator for words (s)
+        int strBeginIndex = 0;
+        while(i< pattern.size() && j<  s.size()){
             
-                std::cout << temp << endl;
-                // Check hashmap1
-                auto hash1 = char_to_word_map.find(*current_char);
-                if(hash1 != char_to_word_map.end()){ // If found
-                    if(char_to_word_map[*current_char] != temp){
-                        return false;
-                    }
+            if(s[j] == ' ' || j == s.size() - 1){ // If we are at the end or found a space, that's a word
+                std::string tempStr(s, strBeginIndex, j - strBeginIndex );
+                if(j == s.size() - 1){ // Get the last character in string s
+                    tempStr += s[j];
                 }
-                else{
-                    char_to_word_map[*current_char] = temp;
-                   
-                }
-                // If exist?
-                auto hash2 = word_to_char_map.find(temp);
-                if(hash2!= word_to_char_map.end()){ // Exists!
-                    if(word_to_char_map[temp] != *current_char){
-                        return false;
-                    }
-                }
-                else{ // Does not exist
-                    word_to_char_map[temp] = *current_char;
-                }
-                j +=2;
-                i = j;
-                current_char++;
+                //std::cout << tempStr << std::endl;
 
+
+                // Checking the hashmap here:
+
+                auto find1 = hashmap1.find(pattern[i]);
+
+                if(find1 != hashmap1.end()){ // Charcter is already mapped
+                    if(find1->second != tempStr ){
+                        return false;
+                    }
+                } 
+                else{ // Character has not been mapped before
+                    hashmap1[pattern[i]] = tempStr; // Mapping the character to the word
+                }
+
+                // Check if the word is already mapped
+                auto find2 = hashmap2.find(tempStr);
+                if(find2 != hashmap2.end()){ // Word is already mapped              
+                    //std::cout << "test 2" << std::endl;
+                    // Check if the current character is equal to what is mapped
+                    if(find2 -> second != pattern[i]){
+                        return false;
+                    }
+                }
+                else{ // If the word have not been mapped
+                    hashmap2[tempStr] = pattern[i];
+                }
+                strBeginIndex = j + 1;
+                i+=1;
             }
-            // Case 2: j is not at "_", keep incrementing
-            else{
-                j++; // Move on to the next character in the string
-            }
+            j+=1;
         }
-        if(current_char != last_char ||  j != char_to_word_end+1){ // Considers the case where there is more characters or words than the other
+
+        // Conditon here to Make sure both reached the end
+        if(i != pattern.size() || j != s.size()){
             return false;
         }
         return true;
