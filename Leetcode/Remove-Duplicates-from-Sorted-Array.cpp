a884fa39b314a188a150531b6ffaf78313f832b4
HaomingMei <137435133+HaomingMei@users.noreply.github.com>
2025-07-14 11:06:25 -0500

    [26] [Time Beats: 100.00%] [Memory Beats: 78.71%] - LeetPush

--- Leetcode/Remove-Duplicates-from-Sorted-Array.cpp
+++ Leetcode/Remove-Duplicates-from-Sorted-Array.cpp
@@ -1,16 +1,35 @@
+#include <set>
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-         if (nums.empty()){
-            return 0; 
-         } 
+        
 
-        int j = 0;
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[j] != nums[i]) {
-                nums[++j] = nums[i]; 
+        // Ascending Int Array
+            // Set data structure  
+        int numUnique = 1;
+        int prevValue = nums[0];
+       // int currentValue;
+     //   if (nums.size() > 0){
+     //       numUnique = 1;
+     //       prevValue = nums[0];
+      //  }
+     //   else{
+        //    return 0;
+     //   }
+        for(int i = 1; i < nums.size(); i++){
+            //currentValue = nums[i];
+            if (prevValue != nums[i]){
+               // std::cout << currentValue << std::endl;
+                nums[numUnique] = nums[i];
+                numUnique += 1;
+                prevValue = nums[i];
             }
+           
         }
-        return j + 1; 
+        return numUnique;
+
+        // No Duplicates & Return the number of unique elements
+
+        // Return Size of the Data Structure
     }
 };
