075fac4a7229ef71e218e5696bc84dedc1db3071
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-07-23 18:47:02 -0500

    [933] [Time Beats: 86.35%] [Memory Beats: 35.92%] - LeetPush

--- Leetcode/Number-of-Recent-Calls.cpp
+++ Leetcode/Number-of-Recent-Calls.cpp
@@ -1,49 +1,20 @@
 #include <queue>
 class RecentCounter {
 public:
-    std::queue<int> myqueue;
+    queue<int> q;
     RecentCounter() {
-        lastReqCount = 0;
+       
     }
     
     int ping(int t) {
-        if(myqueue.empty()){
-            lastReqCount += 1;
-            myqueue.push(t);
-            return lastReqCount;
-        }
-        int last_req = myqueue.back();
-        if(last_req < t- 3000){
-            while(!myqueue.empty()){
-                myqueue.pop();
-            }
-            lastReqCount = 1;
-            myqueue.push(t);
-            return lastReqCount;
-        }
-        int temp;
-        // Goal: Remove what is not within the t- 3000 to t
-        while(!myqueue.empty()){
-            temp = myqueue.front();
-            if(temp >= t-3000){
-                lastReqCount +=1;
-                myqueue.push(t);
-                return lastReqCount;
-            }
-            else{
-                lastReqCount -= 1;
-                myqueue.pop();
-            }
-        }
-        myqueue.push(t);
-        lastReqCount +=1;
-        return lastReqCount;
-
+       q.push(t);
+       while(q.front() < t-3000){
+            q.pop();
+       }
+       return q.size();
+       
 
     }
-private:
-
-    int lastReqCount;
 };
 
 /**
