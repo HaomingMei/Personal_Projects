ae81ca1c69b67c0fcb9016b24e5e8c69fd7fd9c9
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-07-23 18:37:20 -0500

    [933] [Time Beats: 5.01%] [Memory Beats: 5.04%] - LeetPush

---
+++ Leetcode/Number-of-Recent-Calls.cpp
@@ -0,0 +1,41 @@
+#include <deque>
+class RecentCounter {
+public:
+    std::deque<int> mydequeue;
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        int counter = 1;
+        if(mydequeue.empty()){
+            mydequeue.push_back(t);
+            return counter;
+        }
+        int last_req = mydequeue.back();
+        int temp;
+        while(mydequeue.front() != last_req){
+            temp = mydequeue.front();
+            if(temp >= t-3000){
+                counter += 1;
+            }
+            mydequeue.pop_front();
+            mydequeue.push_back(temp);
+        }
+        temp = mydequeue.front();
+        if(temp >= t- 3000){
+            counter += 1;
+        }
+        mydequeue.pop_front();
+        mydequeue.push_back(temp);
+
+        mydequeue.push_back(t);
+        return counter;
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
