21bac6f731f9a4ee60937aa119233299e6a84dab
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-08-03 23:59:03 -0500

    [496] [Time Beats: 100.00%] [Memory Beats: 15.09%] - LeetPush

--- Leetcode/Next-Greater-Element-I.cpp
+++ Leetcode/Next-Greater-Element-I.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
             unordered_map<int, int> mp;
-            stack<int> mystack;
+            stack<int> st;
 
             for(int i=nums2.size()-1; i>=0; i--){
-            while(!mystack.empty() && mystack.top() < nums2[i]){
-                mystack.pop();
+            while(!st.empty() && st.top() < nums2[i]){
+                st.pop();
             }
-            if(mystack.empty() || mystack.top() < nums2[i]) mp[nums2[i]] = -1;
-            else mp[nums2[i]] = mystack.top();
-            mystack.push(nums2[i]);
+            if(st.empty() || st.top() < nums2[i]) mp[nums2[i]] = -1;
+            else mp[nums2[i]] = st.top();
+            st.push(nums2[i]);
             }
 
             vector<int> solution;
