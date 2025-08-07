1c28d4db343e898783636ab07b6473d683f31cac
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-08-07 00:22:28 -0500

    [496] [Time Beats: 11.41%] [Memory Beats: 96.58%] - LeetPush

--- Leetcode/Next-Greater-Element-I.cpp
+++ Leetcode/Next-Greater-Element-I.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-            unordered_map<int, int> mp;
-            stack<int> st;
+            vector<int> result;
+            int indicator = 0;
+            for(int i = 0; i <nums1.size(); i++){
+                int temp1 = nums1[i];
+                for(int j = 0; j < nums2.size(); j++){
+                    if(indicator == 1){
+                        if(temp1<nums2[j]){
+                            indicator = 0;
+                            result.push_back(nums2[j]);
+                            break;
+                        }
+                        if(j== nums2.size()-1){
+                            indicator = 0;
+                            result.push_back(-1);
+                        }
+                        continue;
+                    }
+                    if(temp1 == nums2[j]){
+                        indicator = 1;
+                    }
+                    if(j== nums2.size()-1){
+                        indicator = 0;
+                        result.push_back(-1);
+                    }
 
-            for(int i=nums2.size()-1; i>=0; i--){
-            while(!st.empty() && st.top() < nums2[i]){
-                st.pop();
+                    
+                }
             }
-            if(st.empty() || st.top() < nums2[i]) mp[nums2[i]] = -1;
-            else mp[nums2[i]] = st.top();
-            st.push(nums2[i]);
-            }
-
-            vector<int> solution;
-            for(int i=0; i<nums1.size(); i++){
-            solution.push_back(mp[nums1[i]]);
-            }
-
-
-            return solution;
-        
+            return result;
     }
 };
