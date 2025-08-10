9bd14b01b937f76a03f05c9630c4b19df3dc17e5
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-08-09 23:46:59 -0500

    [145] [Time Beats: 100.00%] [Memory Beats: 71.51%] - LeetPush

--- Leetcode/Binary-Tree-Postorder-Traversal.cpp
+++ Leetcode/Binary-Tree-Postorder-Traversal.cpp
@@ -12,8 +12,37 @@
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
+        // This is code for recursive method
+        //vector<int> myTraversal;
+        //postorder(root, myTraversal);
+        //return myTraversal;
+
+        // Code for iterative method:
         vector<int> myTraversal;
-        postorder(root, myTraversal);
+        stack<TreeNode*> myStack;
+        TreeNode* myNode = root;
+        TreeNode* lastVisited = nullptr; // Need this node to ensure that
+        if(myNode == nullptr){
+            return myTraversal;
+        }
+        while(myNode!= nullptr || !myStack.empty()){
+          if(myNode != nullptr){ 
+            myStack.push(myNode);
+            myNode = myNode->left; // Process the left node first
+          }
+          else{ // Goes here when leftmost node is nullptr, and we need to check the right
+            TreeNode* node = myStack.top(); // Since the left most node of the previous parent is nullptr, we need to go back to the parent 
+            // Then, check the right children
+            if(node->right != nullptr && lastVisited != node->right){ // Right Exist Case, and I haven't visited
+                myNode = node->right;
+            }
+            else{ // Right Does not Exist case
+                myTraversal.push_back(node->val);
+                myStack.pop();
+                lastVisited = node;
+            }
+          }
+        }
         return myTraversal;
     }
     void postorder(TreeNode* node, vector<int>& myTraversal){
