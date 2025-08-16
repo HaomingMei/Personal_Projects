08e44c1cfe1534bcd59b768ef060821d8d739810
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-08-16 00:08:11 -0500

    [1448] [Time Beats: 92.37%] [Memory Beats: 83.34%] - LeetPush

---
+++ Leetcode/Count-Good-Nodes-in-Binary-Tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+
+    int goodNodes(TreeNode* root) {
+        return countGood(root, INT_MIN);
+
+    }
+    int countGood(TreeNode* node, int currentMax){
+        if(node == nullptr){
+            return 0;
+        }
+            int localMax = std::max(currentMax, node->val);
+            return ((node->val >= currentMax) ? 1:0) +countGood(node->left, localMax) + countGood(node->right, localMax);
+        
+        
+    }
+};
