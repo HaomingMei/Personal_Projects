2a95b3fc370b46933a2bf50cad10f7ea2054f0d7
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-08-23 19:30:23 -0500

    [191] [Time Beats: 100.00%] [Memory Beats: 80.57%] - LeetPush

--- Leetcode/Number-of-1-Bits.cpp
+++ Leetcode/Number-of-1-Bits.cpp
@@ -3,9 +3,9 @@ class Solution {
 public:
     int hammingWeight(int n) {
         int ans = 0 ;
-        int rep = std::ceil(std::log2(n));
+       // int rep = std::ceil(std::log2(n));
         //std::cout << "Number of repetition: " << rep<< std::endl;
-        for (int i = 0; i<= rep; i++ ){
+        while(n!= 0){
            // std::cout << "Current n is :" << n << std::endl;
             if((n & (0x1)) == 1) { // Rightmost bits
                 ans+=1;
