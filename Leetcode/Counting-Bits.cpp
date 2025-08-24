e3d7603638e8fca36311484ad23471346e33eb6d
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-08-23 21:34:46 -0500

    [338] [Time Beats: 26.17%] [Memory Beats: 35.71%] - LeetPush

--- Leetcode/Counting-Bits.cpp
+++ Leetcode/Counting-Bits.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans;
-        ans.push_back(0);
-        for(int i = 1; i<= n; i++){
+        vector<int> ans(n+1);
+        int filled = 1;
+
+        for(int i = n; i >= 1; i--){
             int numOnes = 0;
             int temp = i;
             int rep = std::ceil(std::log2(i));
@@ -13,7 +14,22 @@ public:
                 }
                 temp>>=1;
             }
-            ans.push_back(numOnes);
+            temp = i;
+            for (int j = 0; j <= rep ; j++){
+                if(ans[temp] == 0 && temp !=0){
+                    ans[temp] = numOnes;
+                    filled +=1;
+                }
+                if((temp & 0x1)==1){
+                    numOnes-=1;
+                }
+                temp>>=1;
+                if(filled == n+1){
+                    return ans;
+                }
+            }
+            
+           // ans.push_back(numOnes);
         }
         return ans;
     }
