188c72967d62c2803f47f9e14e2b7e484d674f4d
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-09-06 17:51:53 -0500

    [39] [Time Beats: 100.00%] [Memory Beats: 90.70%] - LeetPush

--- Leetcode/Combination-Sum.cpp
+++ Leetcode/Combination-Sum.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        std::vector<vector<int>> res;
+        vector<vector<int>> ans;
         vector<int> comb;
+        solve(ans, comb, candidates, target, 0);
+        return ans;
 
-        solve(candidates, res, comb, target, 0, 0);
 
-       
-        return res;
-
-    }
-    void solve(vector<int> & candidates, std::vector<vector<int>> & res, vector<int>&comb, int target, int sum, int index ){
-        int temp = sum;
-       // std::cout << "The vector at index " << index << "is: " << std::endl;
-       // for(int i = 0; i < comb.size(); i++)
-       // {
-       //     std::cout << comb[i] << " ";
-      //  }
-       // std::cout << std::endl;
-        if(temp == target){
-            //comb.push_back(candidates[index]);
-
-            res.push_back(comb);
-            return;
-        }
-        else if(temp> target || index >= candidates.size()){
-            return;
-        }
         
+    }
+    void solve(vector<vector<int>> & ans, vector<int> & comb, vector<int> & candidates, int target, int index){
+        // Base Case 1: Exceeded target
+        if(target < 0){
+            return;
+        }
+        else if(target == 0){
+            ans.push_back(comb);
+            return;
+        }
+        // Base Case 2: No one elements to choose from
+        if(index >= candidates.size()){
+            return;
+        }
 
+        // Choice 1: Reusing the same one
         comb.push_back(candidates[index]);
-        solve(candidates, res, comb, target, sum +candidates[index], index); // Use the current case/repeat
+        solve(ans, comb, candidates, target-candidates[index], index); 
 
-        // Use the next candidate case
+        // Choice 2: Skipping the current
+        //if(index + 1 >= candidates.size()){ // can't do that cause we need index == candidates sometimes to push comb to ans
+          //  return;
+        //}
         comb.pop_back();
-        //comb.push_back(candidates[index+1]);
-        solve(candidates, res, comb, target, sum, index+1);
-
-
+        solve(ans, comb, candidates, target, index+1);
 
     }
 };
