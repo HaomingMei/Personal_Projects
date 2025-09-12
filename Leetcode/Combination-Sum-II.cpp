6b7dabf3116cbcb8fac4fa3b328eb90d057437e7
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-09-12 13:17:22 -0500

    [40] [Time Beats: 100.00%] [Memory Beats: 82.69%] - LeetPush

--- Leetcode/Combination-Sum-II.cpp
+++ Leetcode/Combination-Sum-II.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        std::vector<vector<int>> res;
-        std::sort(candidates.begin(), candidates.end()); // can do this since the order o the vector in the res does not matter
+        vector<vector<int>> ans;
         vector<int> comb;
-        solve(candidates, res, comb, target, 0, 0 );
-        //vector<vector<int>> final_res;
-        //while(!res.empty()){
-          //  final_res.push_back(*res.begin());
-            //res.erase(res.begin());
-       // }
-        return res;
+        std::sort(candidates.begin(), candidates.end());
+        solve(ans, comb, candidates, target, 0);
+        return ans;
     }
-    void solve(vector<int>& candidates, std::vector<vector<int>> &res, vector<int>&comb, int target, int sum, int index){
-        // Base cases
-        if(sum == target){
-           // vector<int> temp(comb.begin(), comb.end());
-            
-           // std::sort(temp.begin(), temp.end());
-            res.push_back(comb);
+
+    void solve(vector<vector<int>> & ans, vector<int> &comb, vector<int>& candidates, int target, int index){
+        if(target==0 ){ 
+            ans.push_back(comb);
             return;
         }
-        else if(sum > target || index >= candidates.size()){
+        else if(target < 0 ){
             return;
         }
+        if(index == candidates.size()){
+            return;
+        }
+
+        // Choice 1: Include the current
         comb.push_back(candidates[index]);
-        // Case where we use the current
-        solve(candidates, res, comb, target, sum + candidates[index], index+1);
+        solve(ans, comb, candidates, target-candidates[index], index+1);
+
+        comb.pop_back(); // Choice 2: Skip the duplicates since the other path considered it already
         
-        comb.pop_back(); // Case where we do not use the current candidate
-        // If we are skipping the current, and the next canadiate is the same, then we skip those as well
-        int temp = candidates[index];
-        int next = index+1;
-        while(next < candidates.size() && temp == candidates[next]){
-            next+=1;
+        while(index+1 < candidates.size() && candidates[index] == candidates[index+1]){
+            index += 1;
         }
-        solve(candidates, res, comb, target, sum, next);
+        solve(ans, comb, candidates, target, index+1);
+
     }
 };
