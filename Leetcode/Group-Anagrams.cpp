36b87dcd29f6075d2b7f593656e6e1e245bdd60b
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-09-17 18:14:12 -0500

    [49] [Time Beats: 25.35%] [Memory Beats: 15.75%] - LeetPush

--- Leetcode/Group-Anagrams.cpp
+++ Leetcode/Group-Anagrams.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
+     struct VectorHash {
+        size_t operator()(const vector<int>& v) const {
+            std::hash<int> hasher;
+            size_t seed = 0;
+            for (int x : v) {
+                // combine hashes
+                seed ^= hasher(x) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+            }
+            return seed;
+        }
+    };
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> map;
-        for(string word: strs) // eat tea -> aet: eat, tea
+        vector<vector<std::string>> ans;
+        std::unordered_map<vector<int>, vector<std::string>, VectorHash> map;
+        for(int i = 0; i < strs.size(); i++)
         {
-            string temp = word;
-            std::sort(word.begin(), word.end());
-            map[word].push_back(temp);
+            string temp = strs[i];
+            vector<int>freq(26,0); // Calculate its freq for each cahr
+            for(int j = 0; j <temp.size(); j++){
+                freq[temp[j]-'a'] +=1;
+            }
+          //  auto p = map.find(freq);
+            //if(p != map.end()){ // If found, append to vector of strings
+                map[freq].push_back(temp);
+            //}
+            //else{
+              //  map[freq] = {temp}; // Else create a new vector and append it
+            //}
         }
-        std::vector<vector<string>> resultVector;
-        for(auto group: map)// aet: eat, tea
-        {
-            resultVector.push_back(group.second);
+        for(auto &item: map){
+            ans.push_back(item.second);
         }
-        return resultVector;
+        return ans;
     }
 };
