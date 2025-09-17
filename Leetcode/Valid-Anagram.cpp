4bfe47a620f70575a17c310015897c184bdc99c9
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-09-17 17:23:45 -0500

    [242] [Time Beats: 100.00%] [Memory Beats: 48.47%] - LeetPush

--- Leetcode/Valid-Anagram.cpp
+++ Leetcode/Valid-Anagram.cpp
@@ -7,15 +7,21 @@ public:
         }
         // Freq of the words are the same (o(n)) insert to unordered map for both
         std::unordered_map<char, int> s_map;
-        std::unordered_map<char, int> t_map;
+        //std::unordered_map<char, int> t_map;
 
         for(int i = 0; i < s.size(); i++){
             s_map[s[i]] += 1;
-            t_map[t[i]] += 1;
+            //t_map[t[i]] += 1;
         }
 
-        for(auto item: s_map){
-            if(item.second != t_map[item.first]){ // if freq different, return false
+        for(int i = 0; i <t.size(); i++){
+            //if(item.second != t_map[item.first]){ // if freq different, return false
+              //  return false;
+            //}
+            s_map[t[i]] -= 1;
+        }
+        for(auto p: s_map){
+            if(p.second != 0){
                 return false;
             }
         }
