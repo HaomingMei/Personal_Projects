28c1909b37527acb225ce59f476da029bc3a04bf
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-09-27 17:58:29 -0500

    [49] [Time Beats: 84.38%] [Memory Beats: 65.98%] - LeetPush

--- Leetcode/Group-Anagrams.cpp
+++ Leetcode/Group-Anagrams.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-    
+        if(strs.empty()){
+            return {};
+        }
         unordered_map<string, vector<string>> map;
-        for(const auto& s:strs)
+        for(string s:strs)
         {
 
-            vector<int>freq(26,0); // Calculate its freq for each cahr
-            for(char ch: s){
-                freq[ch-'a'] +=1;
-            }
-            string key= to_string(freq[0]); // Custom hashing for string
-            for(int i=1;i<26;i++){
-                key+=','+to_string(freq[i]);
-            }
+            string key = s;
+            std::sort(key.begin(), key.end());
+
             map[key].push_back(s);
 
         }
-            vector<vector<string>> ans;
+        vector<vector<string>> ans;
         for(auto &item: map){
             ans.push_back(item.second);
         }
+    
         return ans;
     }
 };
