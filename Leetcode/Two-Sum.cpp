b601c7f5b8f988f1147772eaa2b123e8d3cd1eef
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-09-27 16:55:13 -0500

    [1] [Time Beats: 100.00%] [Memory Beats: 50.73%] - LeetPush

--- Leetcode/Two-Sum.cpp
+++ Leetcode/Two-Sum.cpp
@@ -2,39 +2,31 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
-        int left, right, mid;
-        int val;
-        vector<std::pair<int, int>> temp; // Keeps track of the value and the index
-        for(int i = 0; i < nums.size(); i++){
-            temp.push_back({nums[i],i});
+        vector<std::pair<int,int>> map; // Keeps track of the number and its index
+        for(int i = 0; i <nums.size(); i ++){
+           map.push_back({nums[i],i});
         }
-        std::sort(temp.begin(), temp.end());
+        std::sort(map.begin(),map.end());
 
-        for(int i = 0 ; i + 1 < temp.size(); i ++ ){
-            left = i + 1;
-            right = temp.size() - 1;
-            mid = (left+right)/2;
-            val = temp[i].first;
-            ans.push_back(temp[i].second);
-            while(left<= right && left < temp.size() && right < temp.size()){
-                int sum = val + temp[mid].first;
-                if(sum == target){
-                    ans.push_back(temp[mid].second);
-                    return ans;
-                }
-                else if(sum < target){
-                    left = mid + 1;
-
-                }
-                else{
-                    right = mid - 1;
-                }
-                mid = (left+right)/2;
-
+        int l = 0;
+        int r = map.size()-1;
+ 
+        while(l<r && l < map.size()&& r < map.size()){
+            int sum = map[l].first + map[r].first;
+            if(sum== target){
+                ans.push_back(map[l].second);
+                ans.push_back(map[r].second);
+                return ans;
             }
-            ans.pop_back();
+            else if(sum< target){
+                l+=1;
+            }
+            else{
+                r-=1;
+            }
 
         }
+        
         return ans;
     }
 };
