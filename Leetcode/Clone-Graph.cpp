e4a94e2fd7f90b44a1bb8c87c3632cc738ade55d
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-09-27 15:41:23 -0500

    [133] [Time Beats: 35.93%] [Memory Beats: 77.65%] - LeetPush

---
+++ Leetcode/Clone-Graph.cpp
@@ -0,0 +1,54 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+#include <stdbool.h>
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (node == nullptr){
+            return nullptr;
+        }
+        queue<Node*> q;
+        unordered_map<int, Node*> map; // Keeps track of the visited
+        q.push(node);
+        //map[node->val] = true;
+        map[node->val]= new Node(node->val); // map to its clone
+        //queue<Node*> q2;
+       // q2.push(newHead);
+
+        while(!q.empty() ){
+     
+            Node* currNode =  q.front();
+            q.pop();
+            for(auto neigh: currNode->neighbors){
+                if(map.find(neigh->val) == map.end()){ // not cloned
+                    map[neigh->val] = new Node(neigh->val);
+                    q.push(neigh);
+                }
+                map[currNode->val]->neighbors.push_back(map[neigh->val]); // add to the cloned' neighbors
+            }
+
+
+        }
+        return map[node->val];
+    }
+
+
+};
