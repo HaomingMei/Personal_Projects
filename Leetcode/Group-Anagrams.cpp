2484042838469d81d2c140c03a238821ad0ccfa6
Haoming Mei <137435133+HaomingMei@users.noreply.github.com>
2025-12-29 16:37:45 -0600

    [49] [Time Beats: 51.09%] [Memory Beats: 64.46%] - LeetPush

--- Leetcode/Group-Anagrams.cpp
+++ Leetcode/Group-Anagrams.cpp
@@ -1,24 +1,24 @@
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        if(strs.empty()){
-            return {};
-        }
-        unordered_map<string, vector<string>> map;
-        for(string s:strs)
-        {
-
-            string key = s;
-            std::sort(key.begin(), key.end());
-
-            map[key].push_back(s);
-
-        }
-        vector<vector<string>> ans;
-        for(auto &item: map){
-            ans.push_back(item.second);
-        }
-    
-        return ans;
-    }
-};
+1class Solution {
+2public:
+3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+4        if(strs.empty()){
+5            return {};
+6        }
+7        unordered_map<string, vector<string>> map;
+8        for(string s:strs)
+9        {
+10
+11            string key = s;
+12            std::sort(key.begin(), key.end());
+13
+14            map[key].push_back(s);
+15
+16        }
+17        vector<vector<string>> ans;
+18        for(auto &item: map){
+19            ans.push_back(item.second);
+20        }
+21    
+22        return ans;
+23    }
+24};
